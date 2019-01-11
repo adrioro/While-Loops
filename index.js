8908e27b94dca5da78a595d4125d18457434c445
adrioro <artistsvillage@me.com>
2019-01-11 08:57:46 -0500

    third commit Do While Loop

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-var i = 0
+//While Loop//
+
+var i = 0;
 while (i < 10) {
   document.write(i + "<br>");
   i = i + 1;
 }
+
+document.write("<br>");
+
+//Do While Loop//
+
+var z = 1;
+
+do {
+  document.write("This will be printed 5 times.<br>");
+  z++;
+}
+while (z < 6);
